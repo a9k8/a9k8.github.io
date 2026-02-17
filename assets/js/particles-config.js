ec8bb3fcc10ac2be17948a1bb3a9431f25bfb8f7
a9k8 <47913076+a9k8@users.noreply.github.com>
2026-02-18 01:30:39 +0530

    Update particles-config.js

--- assets/js/particles-config.js
+++ assets/js/particles-config.js
@@ -109,7 +109,7 @@ particlesJS('particles-js',
           "speed": 3
         },
         "repulse": {
-          "distance": 250
+          "distance": 200
         },
         "push": {
           "particles_nb": 4
