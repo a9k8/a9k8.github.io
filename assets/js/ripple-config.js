38cf26f63fea8355b80f264859fcb8daf4bc0e43
a9k8 <47913076+a9k8@users.noreply.github.com>
2026-03-07 00:35:58 +0530

    Update ripple-config.js

--- assets/js/ripple-config.js
+++ assets/js/ripple-config.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
     $('#ripple-bg').ripples({
         resolution: 512,
-        dropRadius: 15,
+        dropRadius: 25, // Increased from 15 for larger ripples
+        perturbance: 0.05, // Added to make ripples a bit deeper (default is 0.03)
         interactive: true
     })
 });
