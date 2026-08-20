7fb8da465a3cea99b067bebf7eb5a175f9fd33ea
a9k8 <47913076+a9k8@users.noreply.github.com>
2026-08-21 02:13:13 +0530

    Delete assets directory

--- assets/js/ripple-config.js
+++
@@ -1,9 +0,0 @@
-// rippes configuration script
-$(document).ready(function () {
-    $('#ripple-bg').ripples({
-        resolution: 512,
-        dropRadius: 25, // Increased from 15 for larger ripples
-        perturbance: 0.05, // Added to make ripples a bit deeper (default is 0.03)
-        interactive: true
-    })
-});
